4b10b4204d01ac60a1387d51bb78b65cde7fdd33
jonathan <jonathan.developpeur@gmail.com>
2022-06-15 16:25:03 +0200

    Added_project_on_github

---
+++ src/main/java/com/openclassroomsproject/microservicegpsUtil/MicroserviceGpsUtilApplication.java
@@ -0,0 +1,12 @@
+package com.openclassroomsproject.microservicegpsUtil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGpsUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceGpsUtilApplication.class, args);
+	}
+}
---
+++ src/test/java/com/openclassroomsproject/microservicegpsUtil/MicroserviceGpsUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassroomsproject.microservicegpsUtil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceGpsUtilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
